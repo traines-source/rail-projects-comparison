f55f426562262716ae47ce5936587c0571266172
Traines <git@traines.eu>
2023-02-25 20:57:04 +0000

    translations

--- generate/generate.sh
+++ generate/generate.sh
@@ -4,5 +4,5 @@ set -e
 pybabel compile --directory=locale
 #pybabel init -l fr -i locale/messages.pot -d locale 
 #pybabel extract --mapping babel.cfg --output-file=locale/messages.pot .
-pybabel update --input-file=locale/messages.pot --output-dir=locale
+pybabel update --input-file=locale/messages.pot --output-dir=locale --no-wrap --no-fuzzy-matching
 python3 ./generate/generate.py
